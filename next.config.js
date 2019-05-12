60b2c798a0152cfc161b4a6bc68a98b61ba88beb
ph1p <phil@capshake.com>
2019-05-13 01:43:22 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,29 +1,12 @@
-require('dotenv').config();
-
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-
 module.exports = {
   target: 'serverless',
   webpack: config => {
-    config.plugins = config.plugins || [];
-
     config.node = {
       fs: 'empty',
       child_process: 'empty',
       net: 'empty',
       tls: 'empty'
     };
-
-    config.plugins = [
-      ...config.plugins,
-
-      // Read the .env file
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true
-      })
-    ];
 
     return config;
   }
